9f3111e72d0e9ad2b939b69780e1cceeec0f4698
ctbjerk1 <clayton.bjerke@hobbylobby.com>
2016-04-05 11:56:19 -0500

    minor

--- Readme.md
+++ Readme.md
@@ -1,3 +1,9 @@
 ## Quick Start
 1. npm i
 2. babel-node index.js
+
+## References
+1. http://www.2ality.com/2015/11/configuring-babel6.html
+
+## Plugin References
+1. https://babeljs.io/docs/plugins/
--- index.js
+++ index.js
@@ -1,51 +1,10 @@
-var sentence = "The quick brown fox jumped over the lazy dog";
+var a = [
+  "Hydrogen",
+  "Helium",
+  "Lithium",
+  "Beryllium"
+];
 
+var a3 = a.map( s => s.length );
 
-var findLongestWord = (str) => {
-
-  let start = 0;
-  let longest = 0;
-  let word = '';
-
-  for(let i = start; i < str.length; i++) {
-    if(str.charAt(i) === ' ') {
-        word = str.substring(start, i);
-        start = i + 1;
-    }
-
-    if(i === str.length - 1) {
-         word = str.substring(start, i + 1);
-    }
-
-    if(word.length > longest) {
-      longest = word.length;
-      word = '';
-    }
-  }
-
-  return longest;
-}
-
-console.log('FOR LOOP: ', findLongestWord(sentence));
-
-var findLongestWord = (str) => {
-  return str
-    .split(' ')
-    .sort(function(a, b){
-        return a.length - b.length;
-    })
-    .pop()
-    .length;
-}
-
-console.log('SORT: ', findLongestWord(sentence));
-
-
-function findLongestWord(str) {
-
-
-
-  return str.length;
-}
-
-console.log('REDUCE: ', findLongestWord('REDUCE: ', sentence));
+console.log(a3);
